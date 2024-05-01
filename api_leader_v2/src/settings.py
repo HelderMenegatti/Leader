2f972226913ac09969bade1ddda0e327c0532b36
Helder Menegatti <heldermenegatti720@gmail.com>
2024-05-01 16:21:44 -0300

    hotfix config

--- api_leader_v2/src/settings.py
+++ api_leader_v2/src/settings.py
@@ -80,18 +80,6 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "leader-api-db",
-#         "USER": "postgres",
-#         "PASSWORD": "Ratogordo720",
-#         "HOST": "localhost",
-#         "PORT": 5432,
-#     }
-# }
-
-
 
 DATABASES = {
     "default": {
